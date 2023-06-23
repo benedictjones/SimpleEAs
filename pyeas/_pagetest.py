15a85dc4eb27b89b414119210df9e8a2a1c0181b
benedictjones <benedictj@hotmail.co.uk>
2023-06-23 17:18:14 +0100

    Created branch and made bage test file

---
+++ pyeas/_pagetest.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+from typing import Any
+from typing import cast, Union
+from typing import Optional  # telling the type checker that either an object of the specific type is required, or None is required
+import time
